7f6cd2c7ad6d0387b2b1be7904d0a967e9d87a44
Ayesh Vininda <ayeshgk@gmail.com>
2024-04-28 10:23:36 +0530

    claim sub

--- src/main/java/org/example/chatapp/controller/ChatController.java
+++ src/main/java/org/example/chatapp/controller/ChatController.java
@@ -3,20 +3,24 @@ package org.example.chatapp.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.example.chatapp.dto.ChatInfo;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
+import java.util.Map;
 
 @RestController
 @Slf4j
 @RequestMapping("/api/v1/chat")
 public class ChatController {
     @RequestMapping("/my")
-    public ResponseEntity<ChatInfo> chatInfo(){
-        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        log.info(principal.toString());
+    public ResponseEntity<ChatInfo> chatInfo(@AuthenticationPrincipal Jwt jwt){
+        Map<String, Object> claims = jwt.getClaims();
+        String sub = (String) claims.get("sub");
+        log.info("User sub: {}", sub);
         return ResponseEntity.ok(ChatInfo.builder().chatId("ABC").build());
     }
 
